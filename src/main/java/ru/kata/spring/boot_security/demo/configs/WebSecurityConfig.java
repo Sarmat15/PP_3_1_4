7680abd7f4b0f54792d2c18d8d75f2c204df5bb0
zotov <zotovegor952@gmail.com>
2023-01-14 18:53:59 +0300

    Готовое решение

--- src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -42,7 +42,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     protected PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(12);
+        return new BCryptPasswordEncoder(10);
     }
 
     @Override
